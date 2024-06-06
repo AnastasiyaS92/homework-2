f17dbf5859d211354d8e2387ac8f90cb5cf17b87
AnastasiyaS92 <anastasiya_s@mail.ru>
2024-06-06 17:10:53 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+amount_of_homework_completed = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+time_for_one_task = ( Number_of_hours_spent / amount_of_homework_completed)
+print( "Kурс:",Course_name,', всего задач:',amount_of_homework_completed,', затрачено часов:',Number_of_hours_spent,', среднее время выполнения', time_for_one_task , 'часа')
+
